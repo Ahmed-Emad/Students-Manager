66fe73f15df006fd5ada40c7ef264806a4ff94ca
Ahmed Barakat <ahmadbarakat@Ahmads-MacBook-Pro.local>
2014-05-20 17:01:08 +0200

    Initial Commit

---
+++ Data Structure Project/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Data Structure Project
+//
+//  Created by Ahmed Barakat on 140 / 20 / 14.
+//  Copyright (c) 2014 Ahmed Barakat. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
